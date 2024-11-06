ca4812a980fbb11de242761a3c3290980e78b263
raihanmauladani13 <96103609+raihanmauladani13@users.noreply.github.com>
2024-11-06 17:41:30 +0700

    Update sukses.php

--- sukses.php
+++ sukses.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LOGIN BERHASIL</title>
-</html>
+</head>
 <body>
     <h1>LOGIN ANDA BERHASIL</h1>
 </body>
-</head>
+</html>
